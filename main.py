1448375eb6394143f58683b41c4e433c2529fe50
Daraujo-Soares <144711354+dara-soa@users.noreply.github.com>
2025-01-13 10:49:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,15 @@ class Board:
     def __init__(self, board):
         self.board = board
 
+    def find_empty_cell(self):
+        for row, contents in enumerate(self.board):
+            try:
+                col = contents.index(0)
+                return row, col
+            except ValueError:
+                pass
+        return None
+
 puzzle = [
   [0, 0, 2, 0, 0, 8, 0, 0, 0],
   [0, 0, 0, 0, 0, 3, 7, 6, 2],
